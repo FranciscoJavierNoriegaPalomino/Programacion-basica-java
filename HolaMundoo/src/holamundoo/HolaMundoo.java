596a4c9d722a22c4b94caffa7754c2b1f57217a6
FranciscoJavierNoriegaPalomino <1490318@senati.pe>
2024-04-23 13:25:00 -0500

    first commit

---
+++ HolaMundoo/src/holamundoo/HolaMundoo.java
@@ -0,0 +1,7 @@
+package holamundoo;
+public class HolaMundoo {
+    public static void main(String[] args) {
+        System.out.println("Francisco Javier Noriega Palomino");
+        System.out.println("🖐 Hola Mundo");
+    }           
+}
